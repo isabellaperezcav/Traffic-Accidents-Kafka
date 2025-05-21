31c19275971bea9d8b9087a6d9de554de939e5a7
isabellaperezcav <isabellaperezcav@gmail.com>
2025-05-20 23:03:56 -0500

    Dashboard

--- init_ge.py
+++
@@ -1,9 +0,0 @@
-import great_expectations as gx
-
-# Inicializar el contexto
-context = gx.get_context()
-
-# Crear un directorio de configuración de Great Expectations
-context.init_project()
-
-print("Proyecto de Great Expectations inicializado correctamente.")
